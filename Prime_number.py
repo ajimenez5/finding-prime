8a0e0a934a953b11b6c8c2f9031050697683db62
ajimenez5 <ajimenez5@users.noreply.github.com>
2018-03-10 11:31:47 -0600

    done

---
+++ Prime_number.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 10, 2018
+
+@author: ITAUser
+'''
+num=int(input("Enter a positive number greater than 0: "))
+
+if num > 1:
+ 
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+else:
+   print(num,"is not a prime number")
